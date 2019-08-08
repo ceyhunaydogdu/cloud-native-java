99fdad19ad0046af59ca2a8345256c51e8af01cf
ceyhunaydogdu <aydogdu.ceyhun@gmail.com>
2019-08-08 10:58:45 +0300

    added reservation-service

--- reservations-service/src/main/java/com/ca/samples/cloud/reservationsservice/ReservationsServiceApplication.java
+++ reservations-service/src/main/java/com/ca/samples/cloud/reservationsservice/ReservationsServiceApplication.java
@@ -16,9 +16,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication
