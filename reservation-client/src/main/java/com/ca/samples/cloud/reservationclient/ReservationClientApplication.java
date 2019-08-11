d3d66a3b64f65423a79a5b5074f241b517e110c2
ceyhunaydogdu <aydogdu.ceyhun@gmail.com>
2019-08-11 03:30:34 +0300

    edited properties

--- reservation-client/src/main/java/com/ca/samples/cloud/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/ca/samples/cloud/reservationclient/ReservationClientApplication.java
@@ -2,13 +2,12 @@ package com.ca.samples.cloud.reservationclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-// @EnableEurekaClient
-@EnableDiscoveryClient
+@EnableEurekaClient
+// @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationClientApplication {
 
--- reservations-service/src/main/java/com/ca/samples/cloud/reservationsservice/ReservationsServiceApplication.java
+++ reservations-service/src/main/java/com/ca/samples/cloud/reservationsservice/ReservationsServiceApplication.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -20,14 +19,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-// @EnableEurekaClient
+@EnableEurekaClient
 // The one below also works
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class ReservationsServiceApplication {
 
 	@Autowired
