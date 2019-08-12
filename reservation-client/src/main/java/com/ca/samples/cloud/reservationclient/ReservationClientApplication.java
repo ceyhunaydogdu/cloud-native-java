a28d90d3ec414225669c2ac1f283b9c7d7265c18
ceyhunaydogdu <aydogdu.ceyhun@gmail.com>
2019-08-12 12:15:01 +0300

    enabled hystrix circuit braker

--- reservation-client/src/main/java/com/ca/samples/cloud/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/ca/samples/cloud/reservationclient/ReservationClientApplication.java
@@ -2,15 +2,14 @@ package com.ca.samples.cloud.reservationclient;
 
 import static org.springframework.http.HttpStatus.TOO_MANY_REQUESTS;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -18,24 +17,22 @@ import com.netflix.zuul.exception.ZuulException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-
+@EnableCircuitBreaker
 @EnableZuulProxy
 @EnableEurekaClient
 // @EnableDiscoveryClient
@@ -53,14 +50,18 @@ public class ReservationClientApplication {
 
 }
 
+
 @RestController
 @RequestMapping(path = "/reservations")
 class ReservationApiGatewayRestController {
 
 	private final RestTemplate rtemplate;
 
+	public List<String> saver() {
+		return new ArrayList<>();
+	}
 	
-
+	@HystrixCommand(fallbackMethod = "saver")
 	@GetMapping(value="/names")
 	public List<String> getNames() {
 		//Return type can be String.class or JsonNode.class or Map.class instead we use the one below
